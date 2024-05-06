ab79ebea4068ddad63a68624562ff231c872cf6b
HiImMars <fedorenkorostyslav@gmail.com>
2024-05-06 13:25:08 +0300

    tried kick api

--- src/components/home/MainHome.tsx
+++ src/components/home/MainHome.tsx
@@ -1,3 +1,19 @@
+// import { useEffect } from "react";
+
 export const MainHome = () => {
+  // useEffect(() => {
+  //   const fetchChannels = async () => {
+  //     try {
+  //       const response = await fetch("https://kick.com/api/v1/channels/xqc");
+  //       console.log(response);
+  //       return response;
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   fetchChannels();
+  // }, []);
+
   return <div></div>;
 };
