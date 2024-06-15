4fd2154f73c5449b3078de8fcd043f98e3f63c05
HiImMars <fedorenkorostyslav@gmail.com>
2024-06-15 20:28:15 +0300

    Update Greetings.tsx

--- src/components/greetings/Greetings.tsx
+++ src/components/greetings/Greetings.tsx
@@ -5,9 +5,10 @@ export const Greetings = () => {
     <div className="flex flex-col justify-center items-center absolute left-1/3 bottom-1/3">
       <h1 className="w-full">Hi! My nickname is R1hen</h1>
       <p className="w-full">
-        Here you can find all my favourite PVP builds for New World!
+        There will be New World PVP builds later, but now you can enjoy this 3d
+        knight that is watching your cursor!
       </p>
-      <p className="w-full">*All builds are up to date with season 5</p>
+      {/* <p className="w-full">*All builds are up to date with season 5</p> */}
     </div>
   );
 };
